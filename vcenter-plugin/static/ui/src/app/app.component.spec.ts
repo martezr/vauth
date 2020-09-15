5b76cb585312097bd342274992749fc3ae6bff07
martezr <martez.reed@greenreedtech.com>
2020-09-14 22:24:05 -0500

    Add docker builder

--- vcenter-plugin/static/ui/src/app/app.component.spec.ts
+++
@@ -1,5 +0,0 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
-// TODO: implement JS tests for the sample.
-});
